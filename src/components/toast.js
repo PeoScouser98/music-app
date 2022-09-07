9313da2f6da9107289d353ed502e91f7f3fb8249
quanghiep031 <quanghiep03198@gmail.com>
2022-09-08 01:31:40 +0700

    feat: show toast message

---
+++ src/components/toast.js
@@ -0,0 +1,43 @@
+import { $ } from "../utils/dom-query";
+
+const alert = {
+	success: {
+		style: "alert-success",
+		icon: "bi bi-check-circle",
+	},
+	error: {
+		style: "alert-error",
+		icon: "bi bi-x-lg",
+	},
+	info: {
+		style: "alert-info",
+		icon: "bi bi-info-circle",
+	},
+	warning: {
+		style: "alert-warning",
+		icon: "bi bi-exclamation-triangle",
+	},
+};
+
+const createToast = (status, message) => {
+	/* Create notification */
+	const { style, icon } = alert[status];
+	const toastNotification = document.createElement("div");
+	toastNotification.classList.add("toast", "toast-top", "toast-end", "animate-fadeInOut");
+	toastNotification.innerHTML = /* html */ `
+					<div class="alert ${style}">
+						<div>
+							<i class="${icon}"></i>
+							<span>${message}</span>
+						</div>
+					</div>
+				`;
+
+	/* Toggle message */
+	document.body.appendChild(toastNotification);
+	setTimeout(() => {
+		document.body.removeChild(toastNotification);
+	}, 1500);
+};
+
+export default createToast;
