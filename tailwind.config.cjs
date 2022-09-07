ad594fac476ebf39304188030235b6525c728ecc
quanghiep031 <quanghiep03198@gmail.com>
2022-09-08 01:32:49 +0700

    tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["index.html", "./src/**/*.{html,js}"],
+	content: ["*.html", "./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		screens: {
+			sm: "375px",
+
+			md: "768px",
+
+			lg: "1024px",
+
+			xl: "1366px",
+		},
+		extend: {
+			animation: {
+				fadeInOut: "fadeIn .3s linear, fadeOut .3s 2s linear  forwards",
+				fadeOut: "fadeOut 1s ease-in-out",
+			},
+			keyframes: {
+				fadeIn: {
+					"0%": { opacity: "0", transform: "translateX(50%)" },
+					"100%": { opacity: "1", transform: "translateX(0)" },
+				},
+				fadeOut: {
+					"100%": { transform: "translateY(-200%)", opacity: "0" },
+				},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 };
