4c87ff6e04986302fedb03e8112b60266330d71e
quanghiep031 <quanghiep03198@gmail.com>
2022-09-13 11:10:58 +0700

    feat: signin/signup + refresh token + recover password

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,49 @@
 import instance from "./config";
 
-export const login = (user) => {
-	return instance.post("/signin", user);
+export const login = async (user) => {
+	return await instance.post("/login", user);
 };
 
-export const register = (user) => {
-	return instance.post("/signup", user);
+export const register = async (user) => {
+	return await instance.post("/register", user);
 };
+
+export const getUser = async () => {
+	try {
+		return await instance.get("/user");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const getResetPassword = async (email) => {
+	try {
+		return await instance.post("/forgot-password", email);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const refreshToken = async () => {
+	try {
+		const id = localStorage.getItem("id");
+		const newAccessToken = await instance.get("/refresh-token/" + id);
+		await instance.setAccessToken(newAccessToken);
+		return newAccessToken;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const resetPassword = async (data) => {
+	try {
+		return await instance.post("/reset-password", data);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export function logout() {
+	localStorage.clear();
+	window.location.href = "/#/login";
+}
