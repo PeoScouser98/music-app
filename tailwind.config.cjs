1878c6a812722c3ec0b7c35e330690177afaafd6
quanghiep031 <quanghiep03198@gmail.com>
2022-10-29 21:46:39 +0700

    tailwindcss config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["*.html", "./src/**/*.{html,js}"],
+	content: ["./*.{html,js}", "./src/**/*.{html,js}"],
 	theme: {
+		daisyui: {
+			themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+		},
+		containerQuery: {
+			xs: '120px',
+			sm: '240px',
+			md: '360px',
+		},
+		containerType: {
+			size: 'size',
+		},
 		screens: {
+			xs: {
+				min: "300px",
+				max: "374px"
+			},
 			sm: {
 				min: "375px",
 				max: "767px",
@@ -12,18 +27,21 @@ module.exports = {
 				min: "768px",
 				max: "1023px",
 			},
-
 			lg: {
 				min: "1024px",
 				max: "1365px",
 			},
-
 			xl: { min: "1366px" },
+			xxl: { min: "1920px" },
 		},
 		extend: {
+			colors: {
+				'primary': "var(--p)",
+			},
 			animation: {
-				fadeInOut: "fadeIn .3s linear, fadeOut .3s linear 1s forwards",
-				fadeOut: "fadeOut 1s ease-in-out",
+				fadeInOut: "fadeIn .3s linear, fadeOut 1s linear 1s forwards",
+				jumping: "jumping 1.2s linear infinite",
+				spin: "spin 1.2s ease infinite",
 			},
 			keyframes: {
 				fadeIn: {
@@ -31,10 +49,22 @@ module.exports = {
 					"100%": { opacity: "1", transform: "translateX(0)" },
 				},
 				fadeOut: {
-					"100%": { transform: "translateY(-200%)", opacity: "0" },
+					"100%": { transform: "translateY(-75%)", opacity: "0" },
+				},
+				jumping: {
+					"0%": { height: "10%" },
+					"50%": { height: "50%" },
+					"100%": { height: "10%;" },
+				},
+				spin: {
+					"0%": { transform: "rotate(0turn)" },
+					"100%": { transform: "rotate(1turn)" },
 				},
 			},
 		},
 	},
-	plugins: [require("daisyui")],
+	plugins: [
+		require("daisyui"),
+		require("tailwindcss-container-query"),
+	],
 };
